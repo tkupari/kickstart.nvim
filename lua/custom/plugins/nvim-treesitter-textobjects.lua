6c9116c62ee28a1e871ec2a3ba5bb367663fc390
John Doe <email@example.com>
2025-02-15 16:18:04 +0200

    use treesitter to jump to methods

--- lua/custom/plugins/nvim-treesitter-textobjects.lua
+++ lua/custom/plugins/nvim-treesitter-textobjects.lua
@@ -1,3 +1,28 @@
 return {
   'nvim-treesitter/nvim-treesitter-textobjects',
+
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      textobjects = {
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']m'] = '@function.outer',
+          },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+          },
+          goto_next = {},
+          goto_previous = {},
+        },
+      },
+    }
+  end,
 }
